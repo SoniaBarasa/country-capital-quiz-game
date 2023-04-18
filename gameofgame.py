ba696d32e36b6dc8f75ed3b7111a22d58e0f0cc1
SoniaBarasa <51085296+SoniaBarasa@users.noreply.github.com>
2023-04-18 19:41:07 +0300

    add first version of the game

---
+++ gameofgame.py
@@ -0,0 +1,58 @@
+import random
+
+# define a dictionary of countries and their capitals
+countries_capitals = {
+    "Kenya": "Nairobi",
+    "Uganda": "Kampala",
+    "France": "Paris",
+    "Germany": "Berlin",
+    "Italy": "Rome",
+    "Brazil": "Brasilia",
+    "India": "New Delhi",
+    "Japan": "Tokyo",
+    "Russia": "Moscow",
+    "China": "Beijing",
+    "United Kingdom":"London",
+    "USA": "Washington D.C."
+}
+
+# Function to choose a random country from the dictionary
+def choose_country():
+    country = random.choice(list(countries_capitals.keys()))
+    return country
+
+# Function to check if the guess is correct
+def check_guess(guess, country):
+    capital = countries_capitals[country]
+    if guess.lower() == capital.lower():
+        return True
+    else:
+        return False
+
+# Main game loop
+while True:
+    # Choose a random country
+    country = choose_country()
+
+    # Player 1's turn
+    print("Player 1's turn")
+    guess = input(f"What is the capital of {country}? ")
+    if check_guess(guess, country):
+        print("Correct!")
+    else:
+        print("Incorrect.")
+
+    # Player 2's turn
+    print("Player 2's turn")
+    guess = input(f"What is the capital of {country}? ")
+    if check_guess(guess, country):
+        print("Correct!")
+    else:
+        print("Incorrect.")
+
+    # Ask if the players want to continue playing
+    play_again = input("Do you want to play again? (y/n) ")
+    if play_again.lower() != "y":
+        break
+
+print("Thanks for playing!")
