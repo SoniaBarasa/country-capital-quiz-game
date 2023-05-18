c273da0fcc9b942b910ae0f3efe1f9d4cdfa880a
SoniaBarasa <51085296+SoniaBarasa@users.noreply.github.com>
2023-05-18 09:51:44 +0300

    modify the code so there are no conflicts

--- gameofgame.py
+++ gameofgame.py
@@ -18,3 +18,55 @@ for country in pycountry.countries:
 def choose_country():
     country = random.choice(list(countries_capitals.keys()))
     return country
+
+# Function to check if the guess is correct
+def check_guess(guess, country):
+    capital = countries_capitals[country]
+    if guess.lower() == capital.lower():
+        return True
+    else:
+        return False
+
+# Initialize player scores
+player1_score = 0
+player2_score = 0
+
+# Main game loop
+while True:
+    # Choose a random country
+    country = choose_country()
+
+    # Both Players take turns to play
+    print("Player 1's turn")
+    player1_guess = getpass.getpass(prompt=f"What is the capital of {country}? ")
+
+    print("Player 2's turn")
+    player2_guess = getpass.getpass(prompt=f"What is the capital of {country}? ")
+
+    # Reveal the correct answer
+    capital = countries_capitals[country]
+    print(f"The correct answer is {capital}.")
+
+    # Check the guesses and update the scores
+    if check_guess(player1_guess, country) and check_guess(player2_guess, country):
+        print("Both players guessed correctly!")
+        player1_score += 1
+        player2_score += 1
+    elif check_guess(player1_guess, country):
+        print("Player 1 guessed correctly!")
+        player1_score += 1
+    elif check_guess(player2_guess, country):
+        print("Player 2 guessed correctly!")
+        player2_score += 1
+    else:
+        print("Neither player guessed correctly.")
+        
+    # Print the current scores
+    print(f"Current scores: Player 1: {player1_score}, Player 2: {player2_score}")
+    
+    # Ask if the players want to continue playing
+    play_again = input("Do you want to play again? (y/n) ")
+    if play_again.lower() != "y":
+        break
+
+print("Thanks for playing!")
