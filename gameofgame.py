4e6346732b78bf797e0c658c6a21851ba794817d
SoniaBarasa <51085296+SoniaBarasa@users.noreply.github.com>
2023-05-24 09:43:50 +0300

    modify to print congratulatory message at the end

--- gameofgame.py
+++ gameofgame.py
@@ -30,15 +30,17 @@ def check_guess(guess, country):
         return False
 
 # Initialize player scores
+
+
 player1_score = 0
 player2_score = 0
 
-#Defining the congratulatory message
-congrats_message ="Congratulations Player {winner}, you are the winner!"
+# Defining the congratulatory message
+congrats_message = "Congratulations Player {winner}, you are the winner!"
 
-#defining string showing player's turn
+# defining string showing player's turn
 player_turn_str = "Player {player_num}'s turn"
-#return False
+
 
 
 # Main game loop
@@ -47,30 +49,32 @@ while True:
     country = choose_country()
 
     # Both Players take turns to play
-    #player1 goes first
+    # player1 goes first
     print("Player 1's turn")
-    player1_guess = input(f"What is the capital of {country}? ")
-    player1_guess = getpass.getpass(prompt=f"What is the capital of {country}? ")
+    # player1_guess = input(f"What is the capital of {country}? ")
+    player1_guess = getpass.getpass(
+        prompt=f"What is the capital of {country}? ")
     # player 2 goes next
     print(player_turn_str.format(player_num=2))
-    player2_guess = input(f"What is the capital of {country}? ")
+    # player2_guess = input(f"What is the capital of {country}? ")
     # player 2 goes next
-    print("Player 2's turn")
-    player2_guess = getpass.getpass(prompt=f"What is the capital of {country}? ")
+    #print("Player 2's turn")
+    player2_guess = getpass.getpass(
+        prompt=f"What is the capital of {country}? "
+        )
 
-    ##Reveal the correct answer
+    # Reveal the correct answer
     capital = countries_capitals[country]
     print(f"The correct answer is {capital}.")
 
     # Check the guesses and update the scores
-    print(f"The correct answer is {capital}.")
+    # print(f"The correct answer is {capital}.")
 
     # Check the guesses and update the scores
     if check_guess(player1_guess, country) and check_guess(player2_guess, country):
         print("Both players guessed correctly!")
         player1_score += 1
-        player2_score += 1
-        
+        player2_score += 1     
     elif check_guess(player1_guess, country):
         print("Player 1 guessed correctly!")
         print("Well done, you know your Geography well!")
@@ -86,15 +90,20 @@ while True:
         print("You both should learn about capital cities in the world!!!")
         
     # Print the current scores
-    print(f"Current scores: Player 1: {player1_score}, Player 2: {player2_score}")
+    print(
+        f"Current scores: Player 1: {player1_score}, Player 2: {player2_score}"
+        )
     
     # Ask if the players want to continue playing
     play_again = input("Do you want to play again? (y/n) ")
-    if play_again.lower() != "y":
+    if play_again.lower() != "n":
         break
-    
-else:
-    print("The game ended in a tie. Well played both players!")
-    
-print("Thanks for playing!")
-   
+    # Determine the winner and print the congratulatory message
+    if player1_score > player2_score:
+        winner_message = congrats_message.format(winner=1)
+    elif player2_score > player1_score:
+        winner_message = congrats_message.format(winner=2) 
+    else:
+        winner_message = "The game ended in a tie. Well played both players!"
+    print(winner_message)
+    print("Thanks for playing!")
