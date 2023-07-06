1cb04b9e00dff84713f03f5ff6866425b99a4f5b
SoniaBarasa <51085296+SoniaBarasa@users.noreply.github.com>
2023-07-06 14:12:14 +0300

    Commit changes on the test_gameofgame

---
+++ test_gameofgame.py
@@ -0,0 +1,31 @@
+import pytest
+import gameofgame
+
+
+# define test function
+def test_check_guess():
+    # create test data
+    guess = "Nairobi"
+    wrong_guess = "Bagamoyo"
+    country = "Kenya"
+    countries_capitals = {"Kenya": "Nairobi", "Tanzania": "Dodoma"}
+
+    # run function being tested and make assertions
+    assert gameofgame.check_guess(guess, country, countries_capitals)
+    assert not gameofgame.check_guess(wrong_guess, country, countries_capitals)
+
+    # define second test function
+
+
+def test_get_countries_capitals():
+    test_data = {"Uganda": "Kampala", "Egypt": "Cairo"}
+    result = gameofgame.get_countries_capitals()
+
+    assert isinstance(result, dict)
+
+    assert result["Kenya"] == "Nairobi"
+
+    # try  asserting that the test_data is a subset of the result
+
+    for country, capital in test_data.items():
+        assert result[country] == capital
